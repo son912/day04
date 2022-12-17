7058e3deb113b6978f43edd036c4045943580512
son912 <buituanson_t64@hus.edu.vn>
2022-12-17 14:44:34 +0700

    Add new file

--- day04.php
+++ day04.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng ký sinh viên KHTN</title>
+    <title>Đăng ký sinh viên</title>
 
     <script type="text/javascript" src='https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.3.min.js'></script>
 
